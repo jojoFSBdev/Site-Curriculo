2356e64385dfea17c6535b55f9599a9aa6726173
jojoFSB.dev <joaquimfsb.si@gmail.com>
2024-09-12 12:56:47 -0300

    ee

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+function Cadastrar() {
+    //Pega os valores inseridos nos campos de cadastro do site
+    var email = document.querySelector('#INPT_Email').value;
+    var confirmEmail = document.querySelector('#INPT_ConfirmEmail').value;
+    var senha = document.querySelector('#INPT_Senha').value;
+    var confirmSenha= document.querySelector('#INPT_ConfirmSenha').value;
+
+    //Verificar o email e senha inseridos esta igual nos dois campos
+    
+    if ((email != confirmEmail) || (senha != confirmSenha)){
+        window.alert("Algo de Errado não esta certo!")
+    } else {
+       window.open("../HTML/login.html") 
+    }
+
+    console.log(email, senha, confirmEmail, confirmSenha)
+}
